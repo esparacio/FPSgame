2a621ec12b1273f411ca6e46fe6589b841762279
Patrick Lathan <plathan@elon.edu>
2016-10-13 20:11:02 -0400

    added fireball and player detection

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public float spawnTime = 3f;
+    public GameObject Fireball;
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+
+		InvokeRepeating ("DetectPlayer", spawnTime, spawnTime);
+		player = GameObject.Find ("Player");
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void DetectPlayer(){
+        //print("detecting player");
+
+        RaycastHit hit;
+        Vector3 rayDirection = player.transform.position - this.transform.position;
+        Ray ray = new Ray(this.transform.position, rayDirection);
+
+        if (Physics.Raycast(ray, out hit)) {
+            //Debug.Log(hit.collider.ToString());
+            if (hit.collider.tag.Equals("Player")) {
+                GameObject fireballObject = Instantiate(Fireball) as GameObject;
+                fireballObject.transform.position = this.transform.position;
+                fireballObject.GetComponent<Rigidbody>().velocity = Vector3.Normalize(rayDirection)*10;
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fireball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision coll) {
+        GameObject collidedWith = coll.gameObject;
+        Debug.Log("collided");
+        if (collidedWith.tag != "enemy") {
+            Destroy(this.gameObject);
+            if (collidedWith.tag == "player") {
+                //remove health
+            }
+        }
+    }
+}
